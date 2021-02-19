2cfac5fa8b85ec3c6c2f35de2abc1bdae11b654a
xianxianss <75553503+xianxianss@users.noreply.github.com>
2021-02-19 23:04:05 +0800

    放上腳本

---
+++ 2D_GostGame_0207/Assets/Scripts/CameraCControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraCControl : MonoBehaviour
+{
+    [Header("目標物件")]
+    public Transform target;
+    
+    [Header("速度"), Range(0, 1000)]
+    public float speed = 1.5f;
+
+    
+    /// <summary>
+    /// 追蹤目標物件
+    /// </summary>
+    private void Track()
+    {
+        Vector3 posA = target.position;
+        Vector3 posB = transform.position;
+        posA.z = -10;
+        posB = Vector3.Lerp(posA, posB, 0.5f * speed * Time.deltaTime);
+        transform.position = posB;
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+
+}
---
+++ 2D_GostGame_0207/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed;
+
+    public float h;
+    private Rigidbody2D rig;
+
+    #endregion
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        GetHorizontal();
+    }
+
+
+    #region 方法
+
+    private void GetHorizontal()
+    {
+        h = Input.GetAxis("Horizontal");
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.localEulerAngles = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+
+
+
+
+        #endregion
+    }
